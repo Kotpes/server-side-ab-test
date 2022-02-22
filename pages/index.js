d543a97eceda15941ce8bb8204e967b8d98ddf08
Kotpes <kotpes@gmail.com>
2022-02-22 16:50:10 +0200

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,36 @@ export default function Home(props) {
     variantId === '1' ? styles.bVariant : styles.aVariant
   } ${styles.main}`;
   const isAVariant = variantId === '0' || !variantId;
+
   return (
     <div className={styles.container}>
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+            gtag('set', {'experiments': [{'id': ${experimentId}, 'variant': ${variantId}}]});
+          `,
+          }}
+        />
+      </Head>
       <Head>
         <title>AB test page. Variant {isAVariant ? 'A' : 'B'}</title>
       </Head>
