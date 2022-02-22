94a9b7d20bee16f34d7d5c30095521a26a619bb7
Kotpes <kotpes@gmail.com>
2022-02-22 16:43:39 +0200

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -13,33 +13,6 @@ export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-            gtag('set', {'experiments': [{'id': ${experimentId}, 'variant': ${variantId}}]});
-          `,
-          }}
-        />
-      </Head>
-      <Head>
         <title>AB test page. Variant {isAVariant ? 'A' : 'B'}</title>
       </Head>
 
